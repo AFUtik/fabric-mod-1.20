5dc55f93939af8b52a0ae883c8a90bf316c170cb
AFUtik <coffecraft38@gmail.com>
2023-12-25 15:22:34 +0200

    first commit

---
+++ src/main/java/org/callofthevoid/CallOfTheVoid.java
@@ -0,0 +1,13 @@
+package org.callofthevoid;
+
+import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class CallOfTheVoid implements ModInitializer {
+    public static final Logger LOGGER = LogManager.getLogger("callofthevoid");
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/org/callofthevoid/CallOfTheVoidClient.java
@@ -0,0 +1,10 @@
+package org.callofthevoid;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CallOfTheVoidClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
